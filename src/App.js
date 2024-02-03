91e18eaf671aee989c9898b5d469ecff69ec69c3
kavinshah <38910831+kavinshah@users.noreply.github.com>
2024-02-03 12:50:28 +0530

    minor structural changes

--- src/App.js
+++ src/App.js
@@ -8,25 +8,24 @@ class MarkdownPreviewer extends React.Component{
     this.state={
       editorText:'# Welcome to my React Markdown Previewer!\n ## This is a sub-heading...\n### And here\'s some other cool stuff:\n\nHere\'s some code, `<div></div>`, between 2 backticks.\n```\n// this is multi-line code:\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == \'```\' && lastLine == \'```\') {\n    return multiLineCode;\\\n  }\n}\n```\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere\'s also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n\t- Some are bulleted.\n\t\t- With different indentation levels.\n\t\t\t- That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let\'s not forget embedded images:\n![Github Logo](https://cdn.pixabay.com/photo/2014/07/15/23/36/github-394322_1280.png)'
     };
-	this.handleEditorChange=this.handleEditorChange.bind(this);
+      this.handleEditorChange=this.handleEditorChange.bind(this);
   }
   
   handleEditorChange(event){
-    //console.log(event.target.value);
+    console.log(event.target.value);
     this.setState({
       editorText:event.target.value
     });
     marked.use({
       breaks: true,
     });
-    document.getElementById('preview').innerHTML=marked.parse(event.target.value);
   }
   
   render(){
     return (
       <div>
         <Editor content={this.state.editorText} handleEditorChange={this.handleEditorChange} />
-        <Previewer />
+        <Previewer content={this.state.editorText}/>
       </div>
       
     );
@@ -35,7 +34,7 @@ class MarkdownPreviewer extends React.Component{
 
 class Editor extends React.Component{
   render(){
-    //console.log('editor:', this.props.content);
+    console.log('editor:', this.props.content);
     return(
       <div>
         <p>Editor:</p>
@@ -46,6 +45,9 @@ class Editor extends React.Component{
 }
 
 class Previewer extends React.Component{  
+  componentDidMount(){
+    document.getElementById('preview').innerHTML=marked.parse(this.props.content);
+  }
   render(){
     // console.log('previwer:', this.props.content);
     return (
@@ -57,5 +59,7 @@ class Previewer extends React.Component{
     );
   }
 }
+
+ReactDOM.render(<MarkdownPreviewer />, document.getElementById('root'));
 
 export default MarkdownPreviewer;
